d21e9038058189551febc6f626bbda6543cd78ee
kamlesh <kgurjar1947@gmail.com>
2024-06-12 23:52:37 -0700

    captcha added in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,7 +73,7 @@
                                             </div>
                                             @error('password')<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
                                         </div><br>
-                                       <div class="g-recaptcha" id="g-recaptcha" data-sitekey="6LcYwIMpAAAAAFz57MAZJW_B0FRbe8nzY5BbJMup"></div>
+                                       <div class="g-recaptcha" id="g-recaptcha" data-sitekey="6LdfEPcpAAAAAOP5NbJv9ZK10DcW0QRiodWLorF5"></div>
                                         @error('g-recaptcha-response')
                                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                                         @enderror
